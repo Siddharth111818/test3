a3ee45d502c22075c357a815f0089aa492306b90
Shree <shree@gmail.com>
2025-03-19 15:48:17 +0530

    secoun dommit

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test3Application {
 
 	public static void main(String[] args) {
+		String name="mike";
 		int x =100;
+		boolean isPresent=false;
 		SpringApplication.run(Test3Application.class, args);
 	}
 
